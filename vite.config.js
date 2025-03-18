179852d072a054eebf6014e6492d29bd3e1b7236
Caitlin Jones <jones.caitlin.cs@gmail.com>
2025-03-18 15:58:04 -0400

    Index in same dir as app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import path from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
+  root: path.join(__dirname, 'src'),
   plugins: [react()],
+  build: {
+    outDir: path.join(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src', 'index.html'),
+        //nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
 })
